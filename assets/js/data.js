5490a7de58c90290fb192a7a1e05d08e32c6a83e
LujanSolo <lujansolo@proton.me>
2023-06-01 11:47:38 -0700

    added project data to array. added images for new projects

--- assets/js/data.js
+++ assets/js/data.js
@@ -2,7 +2,7 @@ export const projectsArray = [
   {
     id: 1,
     title: "Orbital Odyssey",
-    img: "odyssey.png",
+    img: "assets/images/odyssey.png",
     desc: "Educational app that provides data on the planets of the Solar System, with secure login and client database for note keeping.",
     online: "https://stormy-sands-64982.herokuapp.com/",
     github: "https://github.com/Proper-Stevo/SpaceInvaders",
@@ -10,7 +10,7 @@ export const projectsArray = [
   {
     id: 2,
     title: "Impulse!",
-    img: "impulse.png",
+    img: "assets/images/impulse.png",
     desc: "A full-stack browser-board game where impulse shopping must be leveraged against money and happiness until the finish line is reached.",
     online: "https://impulse-the-game.herokuapp.com/",
     github: "https://github.com/LujanSolo/Impulse",
@@ -18,17 +18,25 @@ export const projectsArray = [
   {
     id: 3,
     title: "The Catered Gamer",
-    img: "catered-gamer.png",
+    img: "assets/images/catered-gamer.png",
     desc: "Discover and re-discover games from all eras based on user's genre preference and type of gaming platform.",
     online: "https://camilleyong.github.io/the-catered-gamer/",
     github: "https://github.com/camilleyong/the-catered-gamer",
   },
   {
     id: 4,
-    title: "The Catered Gamer",
-    img: "catered-gamer.png",
-    desc: "Discover and re-discover games from all eras based on user's genre preference and type of gaming platform.",
-    online: "https://camilleyong.github.io/the-catered-gamer/",
-    github: "https://github.com/camilleyong/the-catered-gamer",
+    title: "Workday Scheduler",
+    img: "assets/images/workday.gif",
+    desc: "Hourly rows from 9am to 5pm help keep you on track through the day. Rows are color coded to represent past/present/future.",
+    online: "https://lujansolo.github.io/workday-scheduler/",
+    github: "https://github.com/LujanSolo/workday-scheduler",
+  },
+  {
+    id: 5,
+    title: "Shout-Out",
+    img: "assets/images/shoutout.png",
+    desc: "A Firebase app where users can give positive feedback to others whom they feel deserve recognition, encouragement, or praise!",
+    online: "https://shoutoutyourfriends.netlify.app/",
+    github: "https://github.com/LujanSolo/shout-out",
   }
 ]
