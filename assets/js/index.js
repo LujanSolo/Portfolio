38c1c7bc195111ee514b90d6c21f7932d03603ab
LujanSolo <lujansolo@proton.me>
2023-06-01 12:25:16 -0700

    added render function and built out getProjectHtml function

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,45 +1,57 @@
 import { projectsArray } from "./data";
 
-let projectHtml = '';
 
-projectHtml += `
-  <div class="project-card">
-  
-    <div class="card-header">
-      <h2 class="card-title">
-        ${project.title}
-      </h2>
-    </div>
+function getProjectHtml(){
+  let projectHtml = '';
 
-    <img
-      src="${project.img}"
-      class="card-image"
-    >
+  projectsArray.forEach((project) =>{
+    projectHtml += `
+      <div class="project-card">
+      
+        <div class="card-header">
+          <h2 class="card-title">
+            ${project.title}
+          </h2>
+        </div>
 
-    <h3 class="card-desc">
-      ${project.desc}
-    </h3>
-
-    <footer class="card-footer">
-      <p>DEPLOYED AT</p>
-      <a href="${project.online}">
         <img
-          class="${project.class}"
-          src="${project.deploy-icon}"
-          alt="icon for the hosting server"
+          src="${project.img}"
+          class="card-image"
         >
-      </a>
 
-      <fr></fr>
+        <h3 class="card-desc">
+          ${project.desc}
+        </h3>
 
-      <p>GITHUB REPO</p>
-      <a href="${project.github}">
-        <img
-          src="${project.deploy-icon}"
-          class="${project-class}"
-          alt="icon for github"
-        >
-      </a>
-    </footer>
-  </div>
-`
+        <footer class="card-footer">
+          <p>DEPLOYED AT</p>
+          <a href="${project.online}">
+            <img
+              class="${project.class}"
+              src="${project.deploy-icon}"
+              alt="icon for the hosting server"
+            >
+          </a>
+
+          <fr></fr>
+
+          <p>GITHUB REPO</p>
+          <a href="${project.github}">
+            <img
+              class="${project.class}"
+              src="${project.deploy-icon}"
+              alt="icon for github"
+            >
+          </a>
+        </footer>
+      </div>
+    `
+  });
+  return projectHtml;
+};
+
+
+function render(){
+  document.getElementById('projects').innerHTML = getProjectHtml();
+}
+
